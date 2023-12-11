ebe5292cf0c82c6a576fef1d0f0d49706a6e45a2
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2023-12-11 20:56:32 +0100

    Day 11 done

--- 2023/Elves/11.jl
+++ 2023/Elves/11.jl
@@ -40,8 +40,29 @@ function get_pairs(a)
     end
     return pairs
 end
-## 
+##
 import LinearAlgebra
-dist(p) = Int(LinearAlgebra.norm(Tuple(p[1] - p[2]), 1))
-# Part 1: 
-sum(map(dist, get_pairs(findall(expanded .== '#'))))
+dist(p1, p2) = Int(LinearAlgebra.norm(p1 .- p2, 1))
+galaxies = map(Tuple, findall(expanded .== '#'))
+##
+# Part 1:
+sum(map(pair -> dist(pair[1], pair[2]), get_pairs(galaxies)))
+##
+num_empty_lines(a, d) = sum(all(s .== '.') for s in eachslice(a, dims=d))
+rows_between(g1, g2, u) = view(u, min(g1[1], g2[1]):max(g1[1], g2[1]), :)
+cols_between(g1, g2, u) = view(u, :, min(g1[2], g2[2]):max(g1[2], g2[2]))
+##
+function enourmous_dist(
+    g1::Tuple{Int,Int}, g2::Tuple{Int,Int}, u::Matrix{Char}, spacing::Int
+)
+    row_spaces = num_empty_lines(rows_between(g1, g2, u), 1)
+    col_spaces = num_empty_lines(cols_between(g1, g2, u), 2)
+    # return dist(g1, g2) + spacing * max(row_spaces - 1, 0) + spacing * max(col_spaces - 1, 0)
+    return (spacing - 1) * (row_spaces + col_spaces) + dist(g1, g2)
+end
+galaxies = map(Tuple, findall(universe .== '#'))
+pairs = get_pairs(galaxies)
+##
+# Part 2
+sum(map(p -> enourmous_dist(p[1], p[2], universe, 1000000), pairs))
+##
