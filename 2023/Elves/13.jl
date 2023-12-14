40be7273936ff7309fbd70bc1f0f1d6dc8ae92b9
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2023-12-14 08:10:54 +0100

    Day 13 part 1+2

--- 2023/Elves/13.jl
+++ 2023/Elves/13.jl
@@ -16,23 +16,36 @@ read_pattern(block)
 blocks = split(replace(read("2023/data/13.txt", String), "\r" => ""), "\n\n")
 patterns = map(read_pattern, blocks)
 ##
-import LinearAlgebra
-function is_reflection(a::AbstractMatrix, b::AbstractMatrix)::Bool
-    n = min(size(a)[2], size(b)[2])
-    left, right = a[:, end-n+1:end], b[:, n:-1:1]
-    return left == right
+function splitborder(a::AbstractArray, c::Int)
+    n = min(c, size(a)[2] - c)
+    left, right = a[:, c-n+1:c], a[:, c+1:c+n]
+    return left, right
+end
+
+num_differences(l, r) = sum(l .⊻ r[:, end:-1:1])
+
+function is_reflection(l::AbstractMatrix, r::AbstractMatrix)::Bool
+    num_differences(l, r) == 0
 end
 is_reflection([0 0 1; 0 0 1; 0 1 0] .== 1, [1 0 0; 1 0 0; 0 1 0] .== 1)
-function get_reflection_border(a::AbstractMatrix, dim::Int)
+##
+function get_reflection_border(a::AbstractMatrix, dim::Int; num_smudges::Int=0)
     if dim == 1
         a = permutedims(a)
     end
     for i in 1:(size(a)[2]-1)
-        left, right = view(a, :, 1:i), @view a[:, i+1:end]
-        if is_reflection(left, right)
+        left, right = splitborder(a, i)
+        if num_differences(left, right) == num_smudges
             return i
         end
     end
     return 0
 end
+##
+# Part 1
 sum(get_reflection_border.(patterns, 2)) + 100 * sum(get_reflection_border.(patterns, 1))
+##
+# Part 2
+sum(get_reflection_border.(patterns, 2, num_smudges=1)) + 100 * sum(get_reflection_border.(patterns, 1, num_smudges=1))
+
+##
