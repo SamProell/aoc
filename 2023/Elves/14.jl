cebb512934ff0fd5f15b8e51fce10c75aa97620b
Samuel Pröll <39363833+SamProell@users.noreply.github.com>
2023-12-15 08:56:45 +0100

    Day 14 part 1

--- 2023/Elves/14.jl
+++ 2023/Elves/14.jl
@@ -2,7 +2,7 @@
 using Revise
 using Elves
 ##
-platform = read_char_array("2023/data/14ex.txt")
+platform = read_char_array("2023/data/14.txt")
 ##
 @enum Direction North East South West
 ## 
@@ -13,26 +13,69 @@ end
 
 BORDER = ' '
 
-function get_neighbor(pos::Position, grid, dir::Direction)
+function get_neighbor_position(pos::Position, grid, dir::Direction)
     if (dir == North) && (pos.y - 1 > 0)
-        return grid[pos.y-1, pos.x]
+        return Position(pos.x, pos.y - 1)
     elseif (dir == East) && (pos.x + 1 <= size(grid)[2])
-        return grid[pos.y, pos.x+1]
+        return Position(pos.x + 1, pos.y)
     elseif (dir == South) && (pos.y + 1 <= size(grid)[1])
-        return grid[pos.y+1, pos.x]
+        return Position(pos.x, pos.y + 1)
     elseif (dir == West) && (pos.x - 1 > 0)
-        return grid[pos.y, pos.x-1]
+        return Position(pos.x - 1, pos.y)
     end
-    return BORDER
+    return nothing
+end
+
+function get_neighbor(pos::Position, grid, dir::Direction)
+    neighbor = get_neighbor_position(pos, grid, dir)
+    if neighbor === nothing
+        return BORDER
+    end
+    return grid[neighbor.y, neighbor.x]
 end
 
 function can_move(pos::Position, grid, dir::Direction)
-    (grid[pos.y, pos.x] == 'O') && (get_neighbor(pos, grid, dir) == ".")
+    (grid[pos.y, pos.x] == 'O') && (get_neighbor(pos, grid, dir) == '.')
+end
+##
+get_neighbor(Position(1, 4), platform, North)
+
+##
+
+function move_pos(pos::Position, grid, dir::Direction)
+    # println(pos, " ", can_move(pos, grid, dir))
+    if can_move(pos, grid, dir)
+        neighbor = get_neighbor_position(pos, grid, dir)
+        grid[pos.y, pos.x] = '.'
+        grid[neighbor.y, neighbor.x] = 'O'
+        return true
+    end
+    return false
 end
 
-function move(pos::Position, grid, dir::Direction)
-    if can_move(pos, grid, dir)
-        ...
+function tilt!(grid, dir::Direction)
+    while true
+        moved = false
+        for y = 1:size(grid)[1], x = 1:size(grid)[2]
+            moved = moved || move_pos(Position(x, y), grid, dir)
+        end
+        if !(moved)
+            break
+        end
     end
 end
+##
+newgrid = copy(platform)
+tilt!(newgrid, North)
+newgrid
+## 
+counts = map(count, eachrow(newgrid .== 'O'))
+##
+function total_weight(grid)
+    counts = map(count, eachrow(newgrid .== 'O'))
+    sum(i * n for (i, n) in enumerate(reverse(counts)))
+end
+##
+# Part 1
+total_weight(newgrid)
 ##
